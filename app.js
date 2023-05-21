861a4195c365bfae711ac443ff0e26e671ff729b
ZuhaibAyaz <zuhaibayaz7@gmail.com>
2023-05-21 16:50:50 +0530

    clickhandler and url updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var btnTranslate= document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv= document.querySelector("#output");
 
-var serverURL="";
+var serverURL="https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(text){
     return  serverURL + "?" + "text=" + text
@@ -12,3 +12,20 @@ function errorHandler(error){
     console.log("error occured", error)
     alert("something went wrong with server! Try again after some time.")
 }
+
+function clickHandler(){
+    var inputText=txtInput.value;  //taking input 
+
+
+//call server for processing
+
+fetch(getTranslationURL(inputText))
+    .then(response =>response.json())
+    .then( json => {
+        var translatedText= json.contents.translated;
+        outputDiv.innerText= translatedText;
+    })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
